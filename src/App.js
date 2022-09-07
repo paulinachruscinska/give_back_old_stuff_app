387e8fa3de1e621acfe6da52866d1bbabce28198
Your Name <you@example.com>
2022-09-07 11:21:34 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import {HashRouter, Route, Routes} from "react-router-dom";
 import Home from "./components/home";
+import Login from "./components/login";
+import Register from "./components/register";
 function App() {
   return (
       <HashRouter>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/logowanie' element={<Login/>}/>
+            <Route path='/rejestracja' element={<Register/>}/>
         </Routes>
       </HashRouter>
 
--- src/components/homeHeader.js
+++ src/components/homeHeader.js
@@ -1,3 +1,35 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
 export default function HomeHeader(){
-    return <h1>Hello1</h1>
+    return (
+        <header className='homeHeader'>
+            <section className='homeHeader__background'></section>
+            <section className='homeHeader__info'>
+                <section className='homeHeader__logIn'>
+                    <Link to='/logowanie' className='homeHeader__logIn--signIn'>Zaloguj</Link>
+                    <Link to='/rejestracja' className='homeHeader__logIn--signUp'>Załóż konto</Link>
+                </section>
+                <nav className='homeHeader__nav'>
+                    <input type='checkbox' className='hamburger'/>
+                    <ul className='homeHeader__nav--ul'>
+                        <li>Start</li>
+                        <li>O co chodzi?</li>
+                        <li>O nas</li>
+                        <li>Fundacja i organizacja</li>
+                        <li>Kontakt</li>
+                    </ul>
+                </nav>
+                <section className='homeHeader__main-section'>
+                    <h2>Zacznij pomagać! <br/>
+                    Oddaj niechciane rzeczy w zaufane ręce</h2>
+                    <div className='homeHeader__main-section--decoration'></div>
+                    <div className='homeHeader__main-section--buttons'>
+                        <Link to='/logowanie' className='button'><span>ODDAJ RZECZY</span></Link>
+                        <Link to='/logowanie' className='button'><span>ZORGANIZUJ ZBIÓRKĘ</span></Link>
+                    </div>
+                </section>
+            </section>
+        </header>
+    )
 }
