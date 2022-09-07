459993cfba674fe791ea785e7359cc248143b3ea
Your Name <you@example.com>
2022-09-07 17:16:53 +0200

    Add 3columns

--- src/components/homeThreeColumns.js
+++ src/components/homeThreeColumns.js
@@ -1,3 +1,37 @@
+const columnText = [
+    {
+        id: 1,
+        number: 10,
+        textPrimary: 'ODDANYCH WORKÓW',
+        textAdditional: 'Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.'
+    },
+    {
+        id: 2,
+        number: 5,
+        textPrimary: 'WSPARTYCH ORGANIZACJI',
+        textAdditional: 'Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.'
+    },
+    {
+        id: 3,
+        number: 7,
+        textPrimary: 'ZORGANIZOWANYCH ZBIÓREK',
+        textAdditional: 'Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.'
+    }
+]
+
 export default function HomeThreeColumns(){
-    return <h1>Hello2</h1>
+    return (
+        <section className="homeThreeColumns">
+            {columnText.map(function (item){
+                return(
+                <div key = {item.id} className='homeThreeColumns__box'>
+                    <p className='homeThreeColumns__box--text-number'>{item.number}</p>
+                    <p className='homeThreeColumns__box--text-primary'>{item.textPrimary}</p>
+                    <p className='homeThreeColumns__box--text-additional'>{item.textAdditional}</p>
+                </div>
+                )
+            })}
+
+        </section>
+    )
 }
