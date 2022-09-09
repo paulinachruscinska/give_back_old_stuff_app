12768e3d1a09b443dbf0ef688106d8e0a17c8e47
Your Name <you@example.com>
2022-09-09 18:37:58 +0200

    Add HomeContactFooter js

--- src/components/homeContactFooter.js
+++ src/components/homeContactFooter.js
@@ -1,3 +1,30 @@
 export default function HomeContactFooter(){
-    return <h1>Hello6</h1>
+    return (
+        <section className='homeContactFooter'>
+            <div className='Contact'>
+                <p className='homeContactFooter__text'>Skontaktuj się z nami</p>
+                <div className='decoration'></div>
+                <form className='homeContactFooter__form'>
+                    <label>
+                        <p className='form__text'>Wpisz swoje imię</p>
+                        <input type='text'/>
+                    </label>
+                    <label>
+                        <p className='form__text'>Wpisz swój email</p>
+                        <input type='email'/>
+                    </label>
+                    <label>
+                        <p className='form__text'>Wpisz swoją wiadomość</p>
+                        <textarea/>
+                    </label>
+                    <input type='submit'/>
+                </form>
+            </div>
+            <div className='footer'>
+                <span className='footer__text'>Copyright by Paulina Chruścińska</span>
+                <img src={require('../assets/Instagram.svg').default} alt='logo-instagram'/>
+                <img src={require('../assets/Facebook.svg').default} alt='logo-facebook'/>
+            </div>
+        </section>
+    )
 }
