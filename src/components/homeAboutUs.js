d1fa89ce4c0edebd0ff2f71d645b4983a5db269a
Your Name <you@example.com>
2022-09-09 13:11:38 +0200

    Add section HomeAboutUs

--- src/components/homeAboutUs.js
+++ src/components/homeAboutUs.js
@@ -1,3 +1,15 @@
 export default function HomeAboutUs(){
-    return <h1>Hello4</h1>
+    return (
+        <section className='homeAboutUs' id='homeAboutUs'>
+            <div className='homeAboutUs__text'>
+                <p className='homeAboutUs__text--primary'>O nas</p>
+                <div className='decoration'></div>
+                <p className='homeAboutUs__text--additional'>Nori grape silver beet broccoli kombu
+                    beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea
+                    prairie turnip leek lentil turnip greens parsnip.</p>
+                <img className='signature' src={require('../assets/Signature.svg').default} alt='signature'/>
+            </div>
+            <div className='homeAboutUs__background'></div>
+        </section>
+    )
 }
