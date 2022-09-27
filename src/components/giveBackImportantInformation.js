eb7bbdd326701b9ad039951b167ccb8637bf9efa
Your Name <you@example.com>
2022-09-27 12:43:53 +0200

    Add JSX

--- src/components/giveBackImportantInformation.js
+++
@@ -1,8 +0,0 @@
-export default function GiveBackImportantInformation(){
-    return(
-        <section className='giveBackImportantInformation'>
-            <p className='giveBackImportantInformation__header'>Ważne!</p>
-            <p className='giveBackImportantInformation__additional'>Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.</p>
-        </section>
-    )
-}
--- src/components/giveback.js
+++ src/components/giveback.js
@@ -1,13 +1,12 @@
 import HomeContactFooter from "./homeContactFooter";
 import GiveBackHeader from "./giveBackHeader";
 import GiveBackSelect from "./giveBackSelect";
-import GiveBackImportantInformation from "./giveBackImportantInformation";
+
 
 export default function GiveBack(){
     return(
         <>
             <GiveBackHeader/>
-            <GiveBackImportantInformation/>
             <GiveBackSelect/>
             <HomeContactFooter/>
         </>
