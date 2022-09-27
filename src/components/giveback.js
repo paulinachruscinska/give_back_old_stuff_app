335c89ddfb076569b0f1f98d6c05c8e6b7b782d1
Paulina <paulina.chruscinska@wp.pl>
2022-09-27 12:44:55 +0200

    Revert "Add JSX"

--- src/components/giveback.js
+++ src/components/giveback.js
@@ -1,12 +1,13 @@
 import HomeContactFooter from "./homeContactFooter";
 import GiveBackHeader from "./giveBackHeader";
 import GiveBackSelect from "./giveBackSelect";
-
+import GiveBackImportantInformation from "./giveBackImportantInformation";
 
 export default function GiveBack(){
     return(
         <>
             <GiveBackHeader/>
+            <GiveBackImportantInformation/>
             <GiveBackSelect/>
             <HomeContactFooter/>
         </>
