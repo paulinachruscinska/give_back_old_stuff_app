248e6dd28752c99e1666a3b6f9ffc99de426b9fc
Your Name <you@example.com>
2022-10-25 11:55:51 +0200

    Add HomeLinks

--- src/components/home.js
+++ src/components/home.js
@@ -4,12 +4,14 @@ import HomeSimpleSteps from "./homeSimpleSteps";
 import HomeAboutUs from "./homeAboutUs";
 import HomeWhoWeHelp from "./homeWhoWeHelp";
 import HomeContactFooter from "./homeContactFooter";
+import {useState} from "react";
 
 
 export default function Home(){
+    const [user, setUser] = useState({})
     return(
         <>
-            <HomeHeader/>
+            <HomeHeader user={user} setUser={setUser}/>
             <HomeThreeColumns/>
             <HomeSimpleSteps/>
             <HomeAboutUs/>
